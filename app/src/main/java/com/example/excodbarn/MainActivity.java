57435fd3690850434cd25bddd784c99634ab376a
dnfatec <danilo@fatecitapetininga.edu.br>
2020-10-01 22:27:17 -0300

    Teste Git Noturno 2020-2

---
+++ app/src/main/java/com/example/excodbarn/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.excodbarn;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+Button btTiraFoto, btLeitura;
+ImageView imgSuaImagem;
+EditText txLeitura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        retiraFoto();
+        lerCodigos();
+    }
+    private void lerCodigos(){
+       btLeitura=(Button)findViewById(R.id.btnLeitura);
+       txLeitura=(EditText)findViewById(R.id.edtLeitura);
+       btLeitura.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent,0);
+           }
+       });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==0){
+            txLeitura.setText(data.getStringExtra("SCAN_RESULT"));
+        }
+        /*Bundle bundle = data.getExtras();
+        if (data!=null){
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+            imgSuaImagem.setImageBitmap(bitmap);
+        }*/
+    }
+
+    private void retiraFoto(){
+        btTiraFoto=(Button)findViewById(R.id.btnTiraFoto);
+        imgSuaImagem=(ImageView)findViewById(R.id.suaImagem);
+        btTiraFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abreCamera= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(abreCamera,0);
+            }
+        });
+    }
+
+}
