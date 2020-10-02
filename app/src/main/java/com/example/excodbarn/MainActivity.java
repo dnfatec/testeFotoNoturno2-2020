106ad887693d904e462dac1bc355ae2dea9e660f
dnfatec <39006449+dnfatec@users.noreply.github.com>
2020-10-01 22:30:24 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/excodbarn/MainActivity.java
+++ app/src/main/java/com/example/excodbarn/MainActivity.java
@@ -21,6 +21,7 @@ EditText txLeitura;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.Maketecfdsfdfdsfsfdsf
         retiraFoto();
         lerCodigos();
     }
